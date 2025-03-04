879e8ab1e59a3755fbcd2cb90584d55aea9f4dd7
Gene <eleonti.it@gmail.com>
2025-03-04 11:30:46 -0500

    Create __init__.py

---
+++ pydungeon/__init__.py
@@ -0,0 +1,11 @@
+"""
+PyDungeon - A Python-based procedural dungeon generator with C++ core.
+
+This package provides tools for generating and visualizing procedural dungeons
+inspired by Diablo's gothic aesthetic.
+"""
+
+from .dungeon_wrapper import DungeonGenerator, benchmark_comparison
+from .visualization import DungeonRenderer
+
+__version__ = "0.1.0"
